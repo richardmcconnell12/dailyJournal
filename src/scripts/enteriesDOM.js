707dacf3b774b6e9f0bceee17b5be139e96b42bf
Ricky McConnell <mr.mcconnell12@yahoo.com>
2019-04-04 17:19:49 -0500

    Part 6 complete with comments added

--- src/scripts/enteriesDOM.js
+++ src/scripts/enteriesDOM.js
@@ -1,15 +1,19 @@
 import data from "./data"
 import entryComponent from "./entryComponent"
 
-const domEntries = {
+// Module that appends list of entries to the DOM.
 
+// Takes data from data module and iterates over to create contact components.
+
+const domEntries = {
+// Function that appends entries to DOM
     appendToDom() {
         console.log("appendToDom")
         data.getJournalEntry()
         .then(allEntries => {
-
+            // Create a document fragment that each entry will append to.
             let largeDocFrag = document.createDocumentFragment();
-
+            // Iterate over each entry
             allEntries.forEach(eachEntry => {
                 console.log("entryData", eachEntry)
                 let entryHtml = entryComponent.createEntryComponent(eachEntry);
--- src/scripts/eventListener.js
+++ src/scripts/eventListener.js
@@ -1,19 +1,24 @@
 import data from "./data"
 
+// Module that contains the eventListener for the "Submit Journal Entry" button.
+
 const eventListener = {
     journalEntryClick() {
+        // Grab the button and adds an event listener to run submissionListener Function.
         console.log("journalEntryCLick")
         let button = document.getElementById("button");
         button.addEventListener("click", eventListener.submissionListener)
     },
 
     submissionListener() {
+        // Function that passes an object and it's value into the API.
         console.log("submissionListener")
         let dateField = document.getElementById("journalDate").value;
         let conceptField = document.getElementById("conceptTopic").value;
         let entryField = document.getElementById("journalEntry").value;
         let moodField = document.getElementById("moodElements").value;
 
+        // Create new object to post to the API.
         let newEntry = {
             date: dateField,
             concept: conceptField,
@@ -21,6 +26,7 @@ const eventListener = {
             mood: moodField
         };
 
+        // Calls the function that passes a new entry to the database.
         data.addJournalEntry(newEntry);
     }
 }
